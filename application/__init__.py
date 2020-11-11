f310d6cdd661adba57c5f1d3ed5177e7293a80da
George-Rhodes <george_rhodes@hotmail.co.uk>
2020-11-11 12:18:11 +0000

    pushing files from other repo

--- application/__init__.py
+++ application/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:dev@ops@35.246.125.154/flask_db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'cheeky key'
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DB_URI')
+app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 db = SQLAlchemy(app)
 
 from application import routes
